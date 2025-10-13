31cbdbd96c57e9dc543fbcbb9eb2a8b87d30437a
DaniLaPandi <0274247@up.edu.mx>
2025-10-13 15:36:29 -0600

    fibonacci js (1 pt)

--- Portfolio/05-JS/js/fibonacci.js
+++ Portfolio/05-JS/js/fibonacci.js
@@ -6,7 +6,7 @@
 var memo = {};
 function fibonacci() {
   "use strict";
-  var n = document.getElementById("num").value;
+  var n = parseInt(document.getElementById("num").value);
   var val = f(n);
   return val;
 }
@@ -18,10 +18,26 @@ function f(n) {
     value = memo[n];
   } else {
     //TODO: Implement the fibonacci function here!
+    if (n == 0) {
+      value = 0;
+    }
+    else if (n == 1) {
+      value = 1;
+    }
+    else {
+      // recursive formula to check for all other nums beside 0 and 1
+      value = f(n - 1) + f(n -2)
+    }
 
     memo[n] = value;
   }
 
   return value;
 }
-console.log(fibonacci(15));
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("btn").addEventListener("click", function() {
+      document.getElementById("fibonacciLbl").textContent = fibonacci();
+  });
+});
+
+
