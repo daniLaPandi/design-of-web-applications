2b60bd21a4a07356fcf10961a3dcf5f622996f5f
DaniLaPandi <0274247@up.edu.mx>
2025-10-13 16:06:58 -0600

    prime js excercise

--- Portfolio/05-JS/js/prime.js
+++ Portfolio/05-JS/js/prime.js
@@ -7,19 +7,20 @@ var getPrimeFactors = function (n) {
   "use strict";
 
   function isPrime(n) {
-    var i;
-
-    for (i = 2; i <= Math.sqrt(n); i++) {
+    for (var i = 2; i <= Math.sqrt(n); i++) {
       if (n % i === 0) {
         return false;
       }
     }
     return true;
   }
-
-  var i,
-    sequence = [];
-
+  
+  var sequence = [];
+  for (var i = 2; i <= n; i++){
+    if (n % i === 0 && isPrime(i)){
+      sequence.push(i);
+    }
+  }
   //TODO: Check which numbers are factors of n and also check if
   // that number also happens to be a prime
 
@@ -27,4 +28,15 @@ var getPrimeFactors = function (n) {
 };
 
 // the prime factors for this number are: [ 2, 3, 5, 7, 11, 13 ]
-console.log(getPrimeFactors(30030));
+// console.log(getPrimeFactors(30030));
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("btn").addEventListener("click", function() {
+    var n = parseInt(document.getElementById("num").value);
+    if (isNaN(n) || n <= 0) {
+      document.getElementById("pf").textContent = "Enter a positive number";
+    } else {
+      document.getElementById("pf").textContent = getPrimeFactors(n).join(", ");
+    }
+  });
+});
