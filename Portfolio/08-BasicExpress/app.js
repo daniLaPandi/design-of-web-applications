2a73931fda5c86690a84890be6eb09aa6612321a
DaniLaPandi <0274247@up.edu.mx>
2025-10-14 23:51:58 -0600

    08 excercise portfolio

---
+++ Portfolio/08-BasicExpress/app.js
@@ -0,0 +1,31 @@
+const express = require('express'); // part of creating an express server
+const bodyParser = require('body-parser'); // this library helps to read raw data and be able to use it in my code
+const path = require('path'); // also needed to create an express server
+
+const app = express(); // part of creating an express server
+const PORT = 3000;  // this is the door that my computer is listening to for network traffic
+
+// middleware: a function that runs before route handler and can modify the request or response
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// in order to serve the static file of index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// post method to handle BMI calculation (requested from the server)
+app.post('/calculate-bmi', (req, res) => {
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+
+    // BMI formula is (weight / height^2) * 10,000
+    const bmi = (weight / (height * height)) * 10000;
+
+    res.send(`<h1>Your BMI is ${bmi.toFixed(2)}</h1>
+              <a href="/">Go back</a>`);
+});
+
+// Start server's listening skills lol
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
