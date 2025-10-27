11d79b502c51eff37acc68e8fc50d872c452a7b8
DaniLaPandi <0274247@up.edu.mx>
2025-10-27 01:44:32 -0600

    *comments made to Node.js portfolio excercise to understand better

--- Portfolio/07-BasicNode/index.js
+++ Portfolio/07-BasicNode/index.js
@@ -1,4 +1,4 @@
-// Star Wars quotes
+// Star Wars quotes library gets imported
 const starWarsQuotes = require("star-wars-quotes");
 
 // superheroes and supervillains
@@ -18,12 +18,13 @@ console.log(starWarsQuotes()); // this function gives a random quote every time 
 console.log("\nEpic Battle!: ", superheroes.randomSuperhero(), " vs. ", supervillains.randomSupervillain());
 
 // read file + secret message
-const filePath = path.join(__dirname, "data", "input.txt");
-
-fs.readFile(filePath, "utf8", (err, data) => {
+const filePath = path.join(__dirname, "data", "input.txt"); // __dirname = is a special Node.js var that contains the this.absolute path of currect file's directory
+// path.join() works independtly of Windows/macOS/Linux
+fs.readFile(filePath, "utf8", (err, data) => { // fs = file system of Node.js for reading/writing files; utf8 is to read it as text instead of as bytes, and
+    // the callback (err, data) means to run when the file is done reading
     if (err) {
         console.error("\nError reading the file: ", err);
-        return;
+        return; // function returns so no attempt is made to read data
     }
-    console.log("\nThe secret message is: ", data);
+    console.log("\nThe secret message is: ", data); // if it works then the file gets read
 });
